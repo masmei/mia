0cd397dc371b7469d6504f1ccd0612667f534250
Mason Mei <masonmei@pursuit.org>
2023-04-29 19:23:40 -0500

    redeployed

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
   async function onSubmit(event) {
     event.preventDefault();
     try {
-      const response = await fetch("/api/generate", {
+      const response = await fetch("https://kaleidoscopic-dango-f3833d.netlify.app/api/generate", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
