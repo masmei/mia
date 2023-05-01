18534f2f1cda5d6d3d53c41a5b7d99d757b75e1e
Mason Mei <masonmei@pursuit.org>
2023-04-30 20:35:41 -0500

    d

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,7 +1,7 @@
 import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: "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",
 });
 const openai = new OpenAIApi(configuration);
 
