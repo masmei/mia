5f4048f18fab2e49846d663eef4373586b3e38dc
Mason Mei <masonmei@pursuit.org>
2023-05-01 00:23:06 -0500

    redeploy

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,70 +1,70 @@
-import { Configuration, OpenAIApi } from "openai";
-
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
-
 export default async function (req, res) {
-  if (!configuration.apiKey) {
+  if (!process.env.OPENAI_API_KEY) {
     res.status(500).json({
       error: {
-        message:
-          "OpenAI API key not configured, please follow instructions in README.md",
-      },
+        message: "OpenAI API key not configured, please follow instructions in README.md",
+      }
     });
     return;
   }
 
-  const {
-    companyName,
-    companyDescription,
-    productDescription,
-    targetAudience,
-  } = req.body;
+  const { companyName, companyDescription, productDescription, targetAudience } = req.body;
+
+  if (!companyName || !companyDescription || !productDescription || !targetAudience) {
+    res.status(400).json({
+      error: {
+        message: "Please provide all required information",
+      }
+    });
+    return;
+  }
 
   try {
-    const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: `Brainstorm 6 original marketing campaigns for ${companyName}, ${companyDescription}. Let your imagination run wild. Provide as much detail as possible and a creative brief for each campaign.
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`,
+      },
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "system",
+            content: "You are an AI assistant that helps users brainstorm marketing campaigns."
+          },
+          {
+            role: "user",
+            content: `Brainstorm 6 original marketing campaigns for ${companyName}, ${companyDescription}. Let your imagination run wild. Provide as much detail as possible and a creative brief.
 
-Product: ${productDescription}
-Target Audience: ${targetAudience}`,
-      temperature: 0.6,
-      max_tokens: 300,
+PRODUCT DESCRIPTION: ${productDescription}
+
+TARGET AUDIENCE: ${targetAudience}`
+          }
+        ],
+        max_tokens: 1000,
+        n: 1,
+        stop: null,
+        temperature: 0.6,
+      }),
     });
 
-    console.log(completion.data.choices[0].text.trim());
-    res.status(200).json({ result: completion.data.choices[0].text.trim() });
+    const data = await response.json();
+    if (response.status !== 200) {
+      throw data.error || new Error(`Request failed with status ${response.status}`);
+    }
+
+    res.status(200).json({ result: data.choices[0].message.content });
   } catch (error) {
-    if (error instanceof openai.error.APIError) {
-      console.error(`OpenAI API returned an API Error: ${error}`);
-      res.status(500).json({
-        error: {
-          message: "An error occurred during your request.",
-        },
-      });
-    } else if (error instanceof openai.error.APIConnectionError) {
-      console.error(`Failed to connect to OpenAI API: ${error}`);
-      res.status(500).json({
-        error: {
-          message: "An error occurred during your request.",
-        },
-      });
-    } else if (error instanceof openai.error.RateLimitError) {
-      console.error(`OpenAI API request exceeded rate limit: ${error}`);
-      res.status(429).json({
-        error: {
-          message:
-            "You have exceeded the rate limit for the OpenAI API. Please wait and try again later.",
-        },
-      });
+    if (error.response) {
+      console.error(error.response.status, error.response.data);
+      res.status(error.response.status).json(error.response.data);
     } else {
-      console.error(`Error with OpenAI API request: ${error}`);
+      console.error(`Error with OpenAI API request: ${error.message}`);
       res.status(500).json({
         error: {
-          message: "An error occurred during your request.",
-        },
+          message: 'An error occurred during your request.',
+        }
       });
     }
   }
--- pages/index.js
+++ pages/index.js
@@ -3,88 +3,116 @@ import { useState } from "react";
 import styles from "./index.module.css";
 
 export default function Home() {
-  const [companyName, setCompanyName] = useState("");
-  const [companyDescription, setCompanyDescription] = useState("");
-  const [productDescription, setProductDescription] = useState("");
-  const [targetAudience, setTargetAudience] = useState("");
+  const [userInput, setuserInput] = useState({
+    companyDescription: "",
+    companyName: "",
+    productDescription: "",
+    targetAudience: "",
+  });
   const [result, setResult] = useState();
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
-  async function onSubmit(event) {
+  const handleTextChange = (event) => {
+    setuserInput({ ...userInput, [event.target.id]: event.target.value });
+  };
+
+  async function sendRequest(event) {
     event.preventDefault();
-    setLoading(true);
     try {
-      const response = await fetch("/api/generate", {
+      setError(false);
+      setLoading(true);
+      const response = await fetch(`/api/generate`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({
-          companyName,
-          companyDescription,
-          productDescription,
-          targetAudience,
-        }),
+        body: JSON.stringify(userInput),
       });
 
       const data = await response.json();
       if (response.status !== 200) {
-        throw data.error || new Error(`Request failed with status ${response.status}`);
+        throw (
+          data.error ||
+          new Error(`Request failed with status ${response.status}`)
+        );
       }
-
-      setResult(data.result);
-    } catch (error) {
-      // Consider implementing your own error handling logic here
-      console.error(error);
-      alert(error.message);
-    } finally {
+      setResult(data.result.split("\n\n"));
       setLoading(false);
+    } catch (error) {
+      console.error(error);
+      setError(true);
     }
   }
 
   return (
     <div>
       <Head>
-        <title>MIA: Marketing Ideation App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>MIA</title>
+        <link rel="icon" href="/comp.png" />
       </Head>
 
       <main className={styles.main}>
-        <h1>MIA: Marketing Ideation App</h1>
-        <form onSubmit={onSubmit}>
+        <h3>Marketing Ideation Assistant</h3>
+        <form onSubmit={sendRequest}>
+          <label htmlFor="companyName">Company Name:</label>
           <input
+            id="companyName"
+            value={userInput.companyName}
             type="text"
-            name="companyName"
-            placeholder="Enter Company Name"
-            value={companyName}
-            onChange={(e) => setCompanyName(e.target.value)}
+            onChange={handleTextChange}
+            placeholder=""
+            required
           />
-          <textarea
-            name="companyDescription"
-            placeholder="Enter Company Description"
-            value={companyDescription}
-            onChange={(e) => setCompanyDescription(e.target.value)}
-          />
-          <textarea
-            name="productDescription"
-            placeholder="Enter Product Description"
-            value={productDescription}
-            onChange={(e) => setProductDescription(e.target.value)}
-          />
+          <label htmlFor="companyDescription">Company Description:</label>
           <input
+            id="companyDescription"
+            value={userInput.companyDescription}
             type="text"
-            name="targetAudience"
-            placeholder="Enter Target Audience"
-            value={targetAudience}
-            onChange={(e) => setTargetAudience(e.target.value)}
+            onChange={handleTextChange}
+            placeholder=""
+            required
           />
-          <input type="submit" value="Generate" disabled={loading} />
+          <label htmlFor="productDescription">Product Description:</label>
+          <textarea
+            id="productDescription"
+            rows={3}
+            cols={50}
+            value={userInput.productDescription}
+            type="text"
+            onChange={handleTextChange}
+            placeholder=""
+            required
+          />
+          <label htmlFor="targetAudience">Target Audience:</label>
+          <textarea
+            id="targetAudience"
+            rows={3}
+            cols={50}
+            value={userInput.targetAudience}
+            type="textarea"
+            onChange={handleTextChange}
+            placeholder=""
+            required
+          />
+          <input type="submit" value="Generate" />
         </form>
-        {loading ? (
-          <div className={styles.loader}></div>
-        ) : (
-          <div className={styles.result}>{result}</div>
-        )}
+        <div className={styles.result}>
+          {loading && <p>Loading...</p>}
+          {error && (
+            <p className={styles.error}>
+              An error occurred during your request. Please try again.
+            </p>
+          )}
+          {!loading &&
+            !error &&
+            result &&
+            result.map((campaign, index) => (
+              <div className={styles.campaign} key={index}>
+                <pre>{campaign}</pre>
+              </div>
+            ))}
+        </div>
       </main>
     </div>
   );
